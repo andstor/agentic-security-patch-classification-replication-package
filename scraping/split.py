1f86f62164542af58d039f390f8a824a12eeb3f7
André Storhaug <andr3.storhaug@gmail.com>
2025-04-08 16:07:05 +0200

    Update splitting

--- scraping/split.py
+++ scraping/split.py
@@ -1,23 +1,39 @@
 import json
+from typing import Any
 from datasets import Dataset, Features, Value, concatenate_datasets, DatasetDict
 from datasets import Features, Value
 from functools import partial
 from tqdm import tqdm
 
-def jsonl_generator(file_path):
+import json
+from datasets import Dataset, Features, Value
+from ftfy import fix_encoding
+from datasets import disable_caching
+from datasets import Features, Value
+from functools import partial
+
+
+num_proc = 20
+shards = [i for i in range(num_proc)]
+
+
+def jsonl_generator(file_path, num_handles=1, shards=[0]):
+    print(f"file_path: {file_path}")
+    rank = shards[0]
     """Yield lines from a JSONL file as dictionaries."""
     with open(file_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            data = json.loads(line)
-            # ensure utf-8 encoding
-            try:
-                #encode commit message to utf-8
-                #data['diff'] = data['diff'].encode('utf-8', 'surrogateescape').decode('ISO-8859-1')
-                data['diff'] = data['diff'].encode('utf-8', "replace").decode('utf-8')
-                yield data
-            except:
-                #print(f"Error encoding line: {line}")
-                raise ValueError(f"Error encoding line")
+        for i, line in enumerate(f):
+            if i % num_handles == rank:
+                data = json.loads(line)
+                # ensure utf-8 encoding
+                try:
+                    #encode commit message to utf-8
+                    #data['diff'] = data['diff'].encode('utf-8', 'surrogateescape').decode('ISO-8859-1')
+                    data['diff'] = data['diff'].encode('utf-8', "replace").decode('utf-8')
+                    yield data
+                except:
+                    #print(f"Error encoding line: {line}")
+                    raise ValueError(f"Error encoding line")
 
 
 def split_numbers_from_dict(data, percent_train=0.80, percent_test=0.20):
@@ -69,23 +85,31 @@ features = Features({
 })
 
 
-dataset_commits = Dataset.from_generator(generator=partial(jsonl_generator, "./output/commits_data.jsonl"), features=features, num_proc=20)
-new_column = [0] * len(dataset_commits)
-dataset_commits = dataset_commits.add_column("label", new_column)
-print(f"Number of non-patching commits: {len(dataset_commits)}")
 
-dataset_patches = Dataset.from_generator(generator=partial(jsonl_generator, "./output/patches_data.jsonl"), features=features, num_proc=20)
+dataset_patches = Dataset.from_generator(generator=partial(jsonl_generator, "./output/patches_data.jsonl"), gen_kwargs={"shards": shards}, features=features, num_proc=num_proc)
 new_column = [1] * len(dataset_patches)
 dataset_patches = dataset_patches.add_column("label", new_column)
 print(f"Number of patcing commits: {len(dataset_patches)}")
 
-dataset = concatenate_datasets([dataset_commits, dataset_patches], axis=0)
-
 # sort the dataset by owner and repo
-dataset = dataset.sort(column_names=['owner', 'repo'], reverse=False)
+dataset_patches = dataset_patches.sort(column_names=['owner', 'repo'], reverse=False)
+
+
+memory = set()
+def is_unique(elem:Any , column: str, memory: set) -> bool:
+    if elem[column] in memory:
+        return False
+    else:
+        memory.add(elem[column])
+        return True
+    
+# deduplicate the dataset by commit_id
+dataset_patches = dataset_patches.filter(partial(is_unique, column="commit_id", memory=memory))
+print(f"Number of unique patcing commits: {len(dataset_patches)}")
+
 
 repos = {} # "repo_owner/repo_name" : count
-for commit in tqdm(dataset):
+for commit in tqdm(dataset_patches):
     repo_ident = commit['owner'] + "/" + commit['repo']
     if repo_ident not in repos:
         repos[repo_ident] = 0
@@ -95,17 +119,51 @@ for commit in tqdm(dataset):
 train_index, test_index = split_numbers_from_dict(repos, percent_train=0.80, percent_test=0.20)
 test_index, validation_index = split_numbers_from_dict(test_index, percent_train=0.50, percent_test=0.50)
 
-train_dataset = dataset.filter(lambda x: (x["owner"] + "/" + x["repo"]) in train_index.keys(), num_proc=20)
-test_dataset = dataset.filter(lambda x: (x["owner"] + "/" + x["repo"]) in test_index.keys(), num_proc=20)
-val_dataset = dataset.filter(lambda x: (x["owner"] + "/" + x["repo"]) in validation_index.keys(), num_proc=20)
+train_dataset_patches = dataset_patches.filter(lambda x: (x["owner"] + "/" + x["repo"]) in train_index.keys(), num_proc=num_proc)
+test_dataset_patches = dataset_patches.filter(lambda x: (x["owner"] + "/" + x["repo"]) in test_index.keys(), num_proc=num_proc)
+val_dataset_patches = dataset_patches.filter(lambda x: (x["owner"] + "/" + x["repo"]) in validation_index.keys(), num_proc=num_proc)
 
 
 ddict = DatasetDict({
-    "train": train_dataset,
-    "test": test_dataset,
-    "validation": val_dataset,
+    "train": train_dataset_patches,
+    "test": test_dataset_patches,
+    "validation": val_dataset_patches,
 })
 
-ddict.push_to_hub("fals3/cvec", config_name="default", private=False, max_shard_size="250MB")
+ddict.push_to_hub("fals3/cvevc3", config_name="patches", private=False, max_shard_size="250MB")
 
 
+
+# Non-patching commits
+# Load the dataset
+
+dataset_non_patches = Dataset.from_generator(generator=partial(jsonl_generator, "./output/commits_data.jsonl"), gen_kwargs={"shards": shards}, features=features, num_proc=num_proc)
+new_column = [0] * len(dataset_non_patches)
+dataset_non_patches = dataset_non_patches.add_column("label", new_column)
+print(f"Number of non-patching commits: {len(dataset_non_patches)}")
+
+dataset_non_patches = dataset_non_patches.sort(column_names=['owner', 'repo'], reverse=False)
+
+train_dataset_non_patches = dataset_non_patches.filter(lambda x: (x["owner"] + "/" + x["repo"]) in train_index.keys(), num_proc=num_proc)
+test_dataset_non_patches = dataset_non_patches.filter(lambda x: (x["owner"] + "/" + x["repo"]) in test_index.keys(), num_proc=num_proc)
+val_dataset_non_patches = dataset_non_patches.filter(lambda x: (x["owner"] + "/" + x["repo"]) in validation_index.keys(), num_proc=num_proc)
+
+ddict = DatasetDict({
+    "train": train_dataset_non_patches,
+    "test": test_dataset_non_patches,
+    "validation": val_dataset_non_patches,
+})
+
+ddict.push_to_hub("fals3/cvevc3", config_name="non_patches", private=False, max_shard_size="250MB")
+
+
+
+# Uplead meta.jsonl
+from huggingface_hub import HfApi
+api = HfApi()
+api.upload_file(
+    path_or_fileobj="./output/meta.jsonl",
+    path_in_repo="meta.jsonl",
+    repo_id="fals3/cvevc3",
+    repo_type="dataset",
+)
