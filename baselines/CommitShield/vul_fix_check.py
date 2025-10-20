b03b7c2a1ebc6815173f95606b4a9edfc2a21fae
André Storhaug <andr3.storhaug@gmail.com>
2025-10-20 16:59:36 +0200

    Correct usage argument in CommitShield

--- baselines/CommitShield/vul_fix_check.py
+++ baselines/CommitShield/vul_fix_check.py
@@ -847,10 +847,10 @@ def all_process(repo_url, usage, args):
                             func_token = 0.3 * len(patch_func[i]['funcs'][j])
                             func_tokens = func_tokens + func_token
                 if func_tokens < 100000:
-                    analyze_answer = LLM_analyze_without_joern(description_pro, patch_new, functions, args)
+                    analyze_answer = LLM_analyze_without_joern(description_pro, patch_new, functions, usage, args)
                 else:
                     functions = ''
-                    analyze_answer = LLM_analyze_without_joern(description_pro, patch_new, functions, args)
+                    analyze_answer = LLM_analyze_without_joern(description_pro, patch_new, functions, usage, args)
                 return analyze_answer
             
     elif len(impact_answer) == 0:
